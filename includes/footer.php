ad43906f607ef480c5e707d870e309bb8b530336
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-06-22 12:47:46 +0100

    Ajout d'un footer stylé et respensive a l'application

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #4482BB;
+            color: white;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <footer>
+        <p>&copy; 2025 - MonPlanning. Tous droits réservés.</p>
+        <p>"Chaque petit pas compte. Continue, tu es sur le bon chemin 🤍"</p>
+    </footer>
+
+</body>
+</html>
