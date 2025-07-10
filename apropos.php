ff9e3e629c9257a536c4a32bc5436dad04cb0c56
dyhiaHachemi <dihya.hachemi@gmail.com>
2025-07-10 14:05:07 +0100

    Ajout et style de la page a propos du site

---
+++ apropos.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>À propos - Mon Planning</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/apropos.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <script src="https://kit.fontawesome.com/a6c1691723.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+
+    <main class="apropos-container">
+        <h1><i class="fa-solid fa-circle-info"></i> À propos</h1>
+        <p>
+        Cette application web a été conçue dans le cadre d’un projet personnel visant à proposer un outil simple de planification et de suivi hebdomadaire.
+        Elle permet de gérer des tâches récurrentes, de suivre leur réalisation quotidienne, et d’évaluer les progrès de manière structurée.
+        </p>
+
+        <p>
+            Les fonctionnalités incluent l’ajout de tâches, leur organisation par catégorie, l’enregistrement des tâches accomplies, 
+            ainsi qu’un résumé quotidien des performances. L’objectif principal est d’optimiser la gestion du temps et de favoriser une meilleure organisation personnelle.
+        </p>
+
+
+        <p class="dev-note">
+            Réalisé avec ❤️ par Dyhia — Étudiante en informatique, passionnée par le web, le développement et l’amélioration personnelle.
+        </p>
+    </main>
+
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
